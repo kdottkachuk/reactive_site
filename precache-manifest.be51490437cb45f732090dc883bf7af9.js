0ed222a6ea04a3405f71153ac3a25c0d0aee8c29
tkonstantin <k.tkachuk@consult-info.ru>
2019-09-05 23:27:37 +0200

    Updates

---
+++ precache-manifest.be51490437cb45f732090dc883bf7af9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3af37e89c9a0bb7781a165f80bed8239",
+    "url": "/reactive_site/index.html"
+  },
+  {
+    "revision": "584ccf37f3858894fb22",
+    "url": "/reactive_site/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "7a7e219d82b7b31d739d",
+    "url": "/reactive_site/static/js/2.51b427a2.chunk.js"
+  },
+  {
+    "revision": "584ccf37f3858894fb22",
+    "url": "/reactive_site/static/js/main.a9d31077.chunk.js"
+  },
+  {
+    "revision": "ad1f8c9456154b7919eb",
+    "url": "/reactive_site/static/js/runtime~main.76ce392b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reactive_site/static/media/logo.5d5d9eef.svg"
+  }
+]);
